73ed9cb9c417d10b6ca5510cda552a765716ddb7
lzhan144 <lzhan144@jhu.edu>
2019-07-09 01:15:36 -0400

    TD3 with Prioritized replay memory

---
+++ TD3_image.py
@@ -0,0 +1,266 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+import utils
+import math
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+# Implementation of Twin Delayed Deep Deterministic Policy Gradients (TD3)
+# Paper: https://arxiv.org/abs/1802.09477
+
+feat_size = 3
+latent_dim = feat_size * feat_size * 128
+
+''' Utilities '''
+class Flatten(torch.nn.Module):
+    def forward(self, x):
+        return x.view(x.size(0), -1)
+
+class Reshape(torch.nn.Module):
+    def __init__(self, outer_shape):
+        super(Reshape, self).__init__()
+        self.outer_shape = outer_shape
+
+    def forward(self, x):
+        return x.view(x.size(0), *self.outer_shape)
+
+
+class Actor(nn.Module):
+    def __init__(self, action_dim, img_stack):
+        super(Actor, self).__init__()
+
+        self.encoder = torch.nn.ModuleList([  ## input size:[96, 96]
+            torch.nn.Conv2d(img_stack, 16, 5, 2, padding=2),   ## output size: [16, 48, 48]
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm2d(16),
+            torch.nn.Conv2d(16, 32, 5 ,2, padding=2),   ## output size: [32, 24, 24]
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm2d(32),
+            torch.nn.Conv2d(32, 64, 5, 2, padding=2),   ## output size: [64, 12, 12]
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm2d(64),
+            torch.nn.Conv2d(64, 128, 5, 4, padding=2),   ## output size: [128, 3, 3]
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm2d(128),
+            Flatten(),   ## output: 1152
+        ])
+
+        self.linear = torch.nn.ModuleList([
+            torch.nn.Linear(latent_dim, 40),
+            torch.nn.ReLU(),
+            torch.nn.Linear(40, 30),
+            torch.nn.ReLU(),
+            torch.nn.Linear(30, action_dim)
+        ])
+
+    def forward(self, x):
+
+        for layer in self.encoder:
+            x = layer(x)
+#             print(x.size())
+        for layer in self.linear:
+            x = layer(x)
+#             print(x.size())
+
+        return x
+
+
+class Critic(nn.Module):
+    def __init__(self, action_dim, img_stack):
+        super(Critic, self).__init__()
+        
+        self.encoder_1 = torch.nn.ModuleList([  ## input size:[224, 224]
+            torch.nn.Conv2d(img_stack, 16, 5, 2, padding=2),   ## output size: [16, 48, 48]
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm2d(16),
+            torch.nn.Conv2d(16, 32, 5 ,2, padding=2),   ## output size: [32, 24, 24]
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm2d(32),
+            torch.nn.Conv2d(32, 64, 5, 2, padding=2),   ## output size: [64, 12, 12]
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm2d(64),
+            torch.nn.Conv2d(64, 128, 5, 4, padding=2),   ## output size: [128, 3, 3]
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm2d(128),
+            Flatten(),   ## output: 1152
+        ])
+        
+        self.encoder_2 = torch.nn.ModuleList([  ## input size:[224, 224]
+            torch.nn.Conv2d(img_stack, 16, 5, 2, padding=2),   ## output size: [16, 48, 48]
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm2d(16),
+            torch.nn.Conv2d(16, 32, 5 ,2, padding=2),   ## output size: [32, 24, 24]
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm2d(32),
+            torch.nn.Conv2d(32, 64, 5, 2, padding=2),   ## output size: [64, 12, 12]
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm2d(64),
+            torch.nn.Conv2d(64, 128, 5, 4, padding=2),   ## output size: [128, 3, 3]
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm2d(128),
+            Flatten(),   ## output: 1152
+        ])
+        
+        self.linear_1 = torch.nn.ModuleList([
+            torch.nn.Linear(latent_dim + action_dim, 40),
+            torch.nn.ReLU(),
+            torch.nn.Linear(40, 30),
+            torch.nn.ReLU(),
+            torch.nn.Linear(30, 1),
+        ])  
+        
+        self.linear_2 = torch.nn.ModuleList([
+            torch.nn.Linear(latent_dim + action_dim, 40),
+            torch.nn.ReLU(),
+            torch.nn.Linear(40, 30),
+            torch.nn.ReLU(),
+            torch.nn.Linear(30, 1),
+        ])
+
+    def forward(self, x, u):       
+        
+        x1 = x
+        for layer in self.encoder_1:
+            x1 = layer(x1)      
+        counter = 0
+        for layer in self.linear_1:
+            counter += 1
+            if counter == 1:
+                x1 = torch.cat([x1, u], 1)
+                x1 = layer(x1)
+            else:
+                x1 = layer(x1)
+        
+        x2 = x
+        for layer in self.encoder_2:
+            x2 = layer(x2)
+        counter = 0
+        for layer in self.linear_2:
+            counter += 1
+            if counter == 1:
+                x2 = torch.cat([x2, u], 1)
+                x2 = layer(x2)
+            else:
+                x2 = layer(x2)
+        
+        return x1, x2
+
+    def Q1(self, x, u):
+        
+        for layer in self.encoder_1:
+            x = layer(x)
+        x1 = x       
+        counter = 0
+        for layer in self.linear_1:
+            counter += 1
+            if counter == 1:
+                x1 = torch.cat([x1, u], 1)
+                x1 = layer(x1)
+            else:
+                x1 = layer(x1)
+
+        return x1
+
+
+class TD3(object):
+    def __init__(self, action_dim,  img_stack):
+
+        self.action_dim = action_dim
+
+        self.actor = Actor(action_dim, img_stack).to(device)
+        self.actor_target = Actor(action_dim, img_stack).to(device)
+        self.actor_target.load_state_dict(self.actor.state_dict())
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters())
+        self.actor_loss = []
+
+        self.critic = Critic(action_dim, img_stack).to(device)
+        self.critic_target = Critic(action_dim, img_stack).to(device)
+        self.critic_target.load_state_dict(self.critic.state_dict())
+        self.critic_optimizer = torch.optim.Adam(self.critic.parameters())
+        self.critic_loss = []
+
+        # self.max_action = max_action
+
+    def select_action(self, state):
+        state = state.float().to(device)
+        return self.actor(state).cpu().data.numpy().flatten()
+
+    def train(self, replay_buffer, iterations, beta_PER, batch_size=100, discount=0.99, tau=0.005, policy_noise=0.2,
+              noise_clip=0.5, policy_freq=2):
+
+        for it in range(iterations):
+
+            # Sample replay buffer
+            x, y, u, r, d, indices, w = replay_buffer.sample(batch_size, beta = beta_PER)
+            state = torch.FloatTensor(x).squeeze(1).to(device)
+#             print('state size: ' +str(state.size()))
+            u = u.reshape((batch_size, self.action_dim))
+            action = torch.FloatTensor(u).to(device)
+#             print('action size: ' +str(action.size()))
+            next_state = torch.FloatTensor(y).squeeze(1).to(device)
+#             print('next state size: ' +str(next_state.size()))
+            done = torch.FloatTensor(1 - d).to(device)
+            reward = torch.FloatTensor(r).to(device)
+            w = w.reshape((batch_size,-1))
+            weights = torch.FloatTensor(w).to(device)
+
+            # Select action according to policy and add clipped noise
+            noise = torch.FloatTensor(u).data.normal_(0, policy_noise).to(device)
+            noise = noise.clamp(-noise_clip, noise_clip)
+            next_action = (self.actor_target(next_state) + noise)
+            next_action[0] = torch.clamp(next_action[0],0,1)
+            next_action[1] = torch.clamp(next_action[1], -1, 1)
+#             print(next_action)
+
+            # Compute the target Q value
+            target_Q1, target_Q2 = self.critic_target(next_state, next_action)
+            target_Q = torch.min(target_Q1, target_Q2)
+            target_Q = reward + (done * discount * target_Q).detach()
+
+            # Get current Q estimates
+            current_Q1, current_Q2 = self.critic(state, action)
+
+            # Compute critic loss
+            critic_loss =  weights * ((current_Q1 - target_Q).pow(2) + (current_Q2 - target_Q).pow(2))
+            prios = critic_loss + 1e-5 
+            critic_loss = critic_loss.mean()
+            self.critic_loss.append(critic_loss)
+
+            # Optimize the critic
+            self.critic_optimizer.zero_grad()
+            critic_loss.backward()
+#             print("indices len: " + str(len(indices)))
+#             print("prios size:" + str(prios.size()))
+            replay_buffer.update_priorities(indices, prios.data.cpu().numpy())
+            self.critic_optimizer.step()
+
+            # Delayed policy updates
+            if it % policy_freq == 0:
+
+                # Compute actor loss
+                actor_loss = -self.critic.Q1(state, self.actor(state)).mean()
+                self.actor_loss.append(actor_loss)
+
+                # Optimize the actor
+                self.actor_optimizer.zero_grad()
+                actor_loss.backward()
+                self.actor_optimizer.step()
+
+                # Update the frozen target models
+                for param, target_param in zip(self.critic.parameters(), self.critic_target.parameters()):
+                    target_param.data.copy_(tau * param.data + (1 - tau) * target_param.data)
+
+                for param, target_param in zip(self.actor.parameters(), self.actor_target.parameters()):
+                    target_param.data.copy_(tau * param.data + (1 - tau) * target_param.data)
+
+    def save(self, filename, directory):
+        torch.save(self.actor.state_dict(), '%s/%s_actor.pth' % (directory, filename))
+        torch.save(self.critic.state_dict(), '%s/%s_critic.pth' % (directory, filename))
+
+    def load(self, filename, directory):
+        self.actor.load_state_dict(torch.load('%s/%s_actor.pth' % (directory, filename)))
+        self.critic.load_state_dict(torch.load('%s/%s_critic.pth' % (directory, filename)))
---
+++ utils.py
@@ -0,0 +1,95 @@
+import numpy as np
+import torch
+import torch.nn as nn
+
+# Code based on: 
+# https://github.com/openai/baselines/blob/master/baselines/deepq/replay_buffer.py
+
+# Expects tuples of (state, next_state, action, reward, done)
+class ReplayBuffer(object):
+	def __init__(self, max_size=1e6):
+		self.storage = []
+		self.max_size = max_size
+		self.ptr = 0
+
+	def add(self, state, new_state, action, reward, done_bool):
+		data = (state, new_state, action, reward, done_bool)
+		if len(self.storage) == self.max_size:
+			self.storage[int(self.ptr)] = data
+			self.ptr = (self.ptr + 1) % self.max_size
+		else:
+			self.storage.append(data)
+
+	def sample(self, batch_size):
+		ind = np.random.randint(0, len(self.storage), size=batch_size)
+		x, y, u, r, d = [], [], [], [], []
+
+		for i in ind: 
+			X, Y, U, R, D = self.storage[i]
+			x.append(np.array(X, copy=False))
+			y.append(np.array(Y, copy=False))
+			u.append(np.array(U, copy=False))
+			r.append(np.array(R, copy=False))
+			d.append(np.array(D, copy=False))
+
+		return np.array(x), np.array(y), np.array(u), np.array(r).reshape(-1, 1), np.array(d).reshape(-1, 1)
+
+
+class NaivePrioritizedBuffer(object):
+	def __init__(self, capacity, prob_alpha=0.6):
+		self.prob_alpha = prob_alpha
+		self.capacity = capacity
+		self.buffer = []
+		self.pos = 0
+		self.priorities = np.zeros((capacity,), dtype=np.float32)
+
+	def add(self, state, next_state, action, reward, done):
+		state = state.numpy()
+		next_state = next_state.numpy()
+		assert state.ndim == next_state.ndim
+		state = np.expand_dims(state, 0)
+		next_state = np.expand_dims(next_state, 0)
+
+		max_prio = self.priorities.max() if self.buffer else 1.0
+
+		if len(self.buffer) < self.capacity:
+			self.buffer.append((state, next_state, action, reward, done))
+		else:
+			self.buffer[self.pos] = (state, next_state, action, reward, done)
+
+		self.priorities[self.pos] = max_prio
+		self.pos = (self.pos + 1) % self.capacity
+
+	def sample(self, batch_size, beta=0.4):
+		if len(self.buffer) == self.capacity:
+			prios = self.priorities
+		else:
+			prios = self.priorities[:self.pos]
+
+		probs = prios ** self.prob_alpha
+		probs /= probs.sum()
+
+		indices = np.random.choice(len(self.buffer), batch_size, p=probs)
+		samples = [self.buffer[idx] for idx in indices]
+
+		total = len(self.buffer)
+		weights = (total * probs[indices]) ** (-beta)
+		weights /= weights.max()
+		weights = np.array(weights, dtype=np.float32)
+
+		batch = list(zip(*samples))
+		states = np.concatenate(batch[0])
+		actions = batch[2]
+		rewards = batch[3]
+		next_states = np.concatenate(batch[1])
+		dones = batch[4]
+
+		return np.array(states), np.array(next_states), np.array(actions), np.array(rewards).reshape(-1, 1), np.array(dones).reshape(-1, 1), indices, weights
+
+	def update_priorities(self, batch_indices, batch_priorities):
+		for idx, prio in list(zip(batch_indices, batch_priorities)):
+			self.priorities[idx] = prio
+
+	def __len__(self):
+		return len(self.buffer)
+
